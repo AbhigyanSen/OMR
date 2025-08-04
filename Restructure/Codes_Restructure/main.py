60169cee4476c3ddab1c1816cc564fd86364a9ef
AbhigyanSen <abhigyansen16@gmail.com>
2025-08-04 14:54:22 +0530

    CHECKPO: Handling ASSAMOMR
    Commented out the Convert Keys in marked option to hardocode returning the marked key, also commented out everything after markedOption run (ICR part and JSON Formatting part)

--- Restructure/Codes_Restructure/main.py
+++ Restructure/Codes_Restructure/main.py
@@ -2,7 +2,8 @@ from modules.anchorDetection import process_batch
 from modules.fieldMapping import process_field_mapping
 from modules.markedOption import process_marked_options
 from modules.runRequest import process_icr_requests
-import sys, json
+from modules.jsonUtils import json_restructure
+import os, re, sys, json
 
 def load_config(path="config.json"):
     with open(path) as f:
@@ -51,7 +52,46 @@ if __name__ == "__main__":
         f"Images: {marked_stats['processed_images']}, "
         f"Detected fields: {marked_stats['total_detected_fields']}")
 
-    # Process ICR requests for the batch
-    # batch_name.json = os.path.join(base_folder, "Images", omr_template_name, date, "Output", {batch_name}, f"{batch_name}.json")
-    icr_stats = process_icr_requests(base_folder, omr_template_name, date, batch_name)
-    print(f"|INFO| ICR processed images: {icr_stats['processed_images']}")
+    # # Process ICR requests for the batch
+    # # batch_name.json = os.path.join(base_folder, "Images", omr_template_name, date, "Output", {batch_name}, f"{batch_name}.json")
+    # icr_stats = process_icr_requests(base_folder, omr_template_name, date, batch_name)
+    # print(f"|INFO| ICR processed images: {icr_stats['processed_images']}")
+    
+    
+    # ----- Wrishav's utils.py usage -----
+    
+    # json_restructure(base_folder, omr_template_name, date, batch_name)
+    
+    # # Importing base packages
+    # import os
+    # import re
+    # import json
+
+    # # Importing custom packages
+    # from modules.jsonUtils import json_restructure
+
+    # # Path setup for function calls
+    # omr_template_name = omr_template_name
+    # base_folder = "D:\\Projects\\OMR\\new_abhigyan\\Restructure"
+    # output_json_path = os.path.join(base_folder, "Images", omr_template_name, date, "Output", {batch_name}, f"{batch_name}.json")
+    # field_mappings_path = os.path.join(base_folder, "Images", omr_template_name, date, "Output", batch_name, "annotate_" + "batch_name", "field_mappings.json")
+    # key_fields_path = os.path.join(base_folder, "Annotations", omr_template_name, "key_fields.json")
+    # key_fields_coordinates_output_path = os.path.join(base_folder, "Annotations", omr_template_name, "key_fields_coordinates.json")
+
+    # # Function calls
+    # output_file_path = process_omr_data(
+    #     json_file_path=output_json_path,
+    #     key_fields_path=key_fields_path,
+    #     template_name=omr_template_name,
+    #     output_file_path=output_json_path) # Both INPUT and OUTPUT path are same so as to make the changes directly into the original file
+    
+    # coordinates_output_path = process_key_coordinates(
+    #     field_mappings_path=field_mappings_path,
+    #     key_fields_path=key_fields_path,
+    #     output_path=key_fields_coordinates_output_path) # OUTPUT is saved to the same folder as key_fields.json for easier access and error proofing
+
+    # updated_output_file_path=update_key_field_dimensions(
+    #     output_json_path=output_file_path,
+    #     key_field_coordinates_path=coordinates_output_path,
+    #     key_fields_path=key_fields_path
+    # )
